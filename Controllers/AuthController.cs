8cb3e411847731b2853b21941a7e4742a5ded278
yunver <yunus.unver1995@gmail.com>
2019-07-04 22:15:42 +0300

    Added Login ActionResult in AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SehirRehberi.API.Data;
 using SehirRehberi.API.Dtos;
 using SehirRehberi.API.Models;
@@ -24,7 +28,7 @@ namespace SehirRehberi.API.Controllers
             _configuration = configuration;
         }
 
-        [HttpPost("Register")]
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             if (await _authRepository.UserExists(userForRegisterDto.UserName))
@@ -44,6 +48,34 @@ namespace SehirRehberi.API.Controllers
 
             var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
             return StatusCode(201,createdUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UserForLoginDto userForLoginDto)
+        {
+            var user = await _authRepository.Login(userForLoginDto.UserName, userForLoginDto.Password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                    new Claim(ClaimTypes.Name,user.UserName) 
+                }
+                ),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(tokenString);
         }
     }
 }
