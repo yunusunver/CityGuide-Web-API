cd859faa196c0b3a3ae8d8a8c08e6d176cae1d4f
yunver <yunus.unver1995@gmail.com>
2019-07-04 21:12:00 +0300

    Added Login and UserExists Operation

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SehirRehberi.API.Models;
 
 namespace SehirRehberi.API.Data
@@ -37,14 +38,47 @@ namespace SehirRehberi.API.Data
             }
         }
 
-        public Task<User> Login(string userName, string password)
+        public async Task<User> Login(string userName, string password)
         {
-            
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
         }
 
-        public Task<bool> UserExists(string userName)
+        private bool VerifyPasswordHash(string password, byte[] userPasswordHash, byte[] userPasswordSalt)
         {
-            
+            using (var hmac=new System.Security.Cryptography.HMACSHA512(userPasswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != userPasswordHash[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public async Task<bool> UserExists(string userName)
+        {
+            if (await _context.Users.AnyAsync(x=>x.UserName == userName))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
