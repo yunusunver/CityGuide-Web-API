10e0228450004cf1615a4f64af873df79de61772
yunver <yunus.unver1995@gmail.com>
2019-07-04 22:53:08 +0300

    Added PhotoForCreationDto and PhotoForReturnDto in Dto

---
+++ Dtos/PhotoForCreationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SehirRehberi.API.Dtos
+{
+    public class PhotoForCreationDto
+    {
+        public PhotoForCreationDto()
+        {
+            DateAdded=DateTime.Now;
+        }
+        public string Url { get; set; }
+        public IFormFile File { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+    }
+}
---
+++ Dtos/PhotoForReturnDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Dtos
+{
+    public class PhotoForReturnDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string IsMain { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+
+    }
+}
